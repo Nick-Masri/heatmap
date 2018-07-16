8c2763e38176a1389261a7224c90ea1e0dca3900
pense2 <jackschilling@live.com>
2018-07-16 15:36:42 -0700

    fix

--- heatmap.m
+++ heatmap.m
@@ -1,1 +1,11 @@
 % This is how you comment / document
+
+
+% Calculates the color by feeding in the decimal 0-10
+    if decimal >=5
+        blue_value = ((10-decimal)/5)*255;
+        return (255 0 floor(blue_value));
+    else
+        red_value = (decimal/5)*255
+        return (floor(red_value) 0 255)
+    end
